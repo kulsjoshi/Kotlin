c16fd0fc54f6d70160ae3de4f179327c48e09bb2
Kuldeep J <kuls.droid@gmail.com>
2021-05-05 23:37:36 -0700

    - Initial commit for work manager

--- app/src/main/java/com/kuldeepjoshi/kotlintutorial/MyApplication.kt
+++ app/src/main/java/com/kuldeepjoshi/kotlintutorial/MyApplication.kt
@@ -4,12 +4,16 @@ import android.app.Application
 import com.kuldeepjoshi.kotlintutorial.dependencyInjection.appDependencies
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import timber.log.Timber
 
 class MyApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
 
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
 
         startKoin{
             androidContext(this@MyApplication)
